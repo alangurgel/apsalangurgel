e7dbf0df80263a6634a91f2a4e71ee0d36f9a2ec
alangurgel <al4npbs@gmail.com>
2019-04-17 15:46:10 -0300

    setup product image field

--- products/views.py
+++ products/views.py
@@ -28,7 +28,7 @@ class ProductDetailView(DetailView):
     queryset = Product.objects.all()
     template_name = "products/detail.html"
     
-     def get_context_data(self):
+    def get_context_data(self):
         context = super(ProductDetailView, self).get_context_data(*args, **kwargs)
         print(context)
         return context
