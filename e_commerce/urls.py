de69ae4088d4e4295562f3201879bb888e523b45
alangurgel <al4npbs@gmail.com>
2019-04-17 13:22:41 -0300

    restore app

--- e_commerce/urls.py
+++ e_commerce/urls.py
@@ -23,7 +23,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 
-from products.views import ProductListView, product_list_view
+from products.views import ProductListView, product_list_view, ProductDetailView, product_detail_view
 from .views import home_page, about_page, contact_page, login_page, register_page
 
 urlpatterns = [
@@ -34,6 +34,8 @@ urlpatterns = [
         path('register/', register_page),
         path('products/', ProductListView.as_view()),
         path('products-fbv/', product_list_view),    
+        path('products/<int:pk>', ProductDetailView.as_view()),
+        path('products-fbv/<int:pk>', product_detail_view), 
         path('admin/', admin.site.urls),
         
 ]
