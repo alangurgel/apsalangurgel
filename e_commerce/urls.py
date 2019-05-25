bb0817ef6e0e202d5c2ffa0bd0ea187be875f797
alangurgel <al4npbs@gmail.com>
2019-05-25 01:03:12 -0300

    create featured-datail and modificate and detail.htmlg

--- e_commerce/urls.py
+++ e_commerce/urls.py
@@ -23,7 +23,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 
-from products.views import ProductListView, product_list_view, ProductDetailView, product_detail_view
+from products.views import (ProductListView, product_list_view, ProductDetailView, product_detail_view, ProductFeaturedListView, ProductFeaturedDetailView)
 from .views import home_page, about_page, contact_page, login_page, register_page
 
 urlpatterns = [
@@ -32,6 +32,8 @@ urlpatterns = [
 	    path('contact/', contact_page),
         path('login/', login_page),
         path('register/', register_page),
+        path('featured/', ProductFeaturedListView.as_view()),
+        path('featured/<int:pk>/', ProductFeaturedDetailView.as_view()),
         path('products/', ProductListView.as_view()),
         path('products-fbv/', product_list_view),    
         path('products/<int:pk>', ProductDetailView.as_view()),
