6a6fc72f7cddae089fb15c91c56b7b4bfb25a391
alangurgel <al4npbs@gmail.com>
2019-05-25 01:29:05 -0300

    heandle MultipleObjectsReturned except

--- e_commerce/urls.py
+++ e_commerce/urls.py
@@ -23,7 +23,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 
-from products.views import (ProductListView, product_list_view, ProductDetailView, product_detail_view, ProductFeaturedListView, ProductFeaturedDetailView)
+from products.views import (ProductListView, 
+                            product_list_view,
+                            ProductDetailView, 
+                            product_detail_view, 
+                            ProductDetailSlugView, 
+                            ProductFeaturedListView, 
+                            ProductFeaturedDetailView)
+                            
 from .views import home_page, about_page, contact_page, login_page, register_page
 
 urlpatterns = [
@@ -37,7 +44,8 @@ urlpatterns = [
         path('products/', ProductListView.as_view()),
         path('products-fbv/', product_list_view),    
         path('products/<int:pk>', ProductDetailView.as_view()),
-        path('products-fbv/<int:pk>', product_detail_view), 
+        path('products-fbv/<int:pk>', product_detail_view),
+        path('products/<slug:slug>/', ProductDetailSlugView.as_view()), 
         path('admin/', admin.site.urls),
         
 ]
